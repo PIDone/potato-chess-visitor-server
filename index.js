062a286632aa3f71786afcfc2c55e855de7a302c
anonymouscoder999 <jlche18659@gmail.com>
2022-07-05 13:27:44 +0800

    sdf

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const fs = require("fs");
 
+var views = 0;
+
 const app = express();
 
 app.get("/", (req, res) => {
-    fs.readFile("visitors.txt", "utf8", (err, data) => {
-        let after = (parseInt(data) + 1).toString();
-        fs.writeFile("visitors.txt", after, err => { });
-        res.send(after);
-    });
+    views++;
+    // fs.readFile("visitors.txt", "utf8", (err, data) => {
+        // let after = (parseInt(data) + 1).toString();
+        // fs.writeFile("visitors.txt", after, err => { });
+        res.send(views.toString());
+    // });
 });
 app.get("/query", (req, res) => {
-    fs.readFile("visitors.txt", "utf8", (err, data) => {
-        res.send(data);
-    });
+    // fs.readFile("visitors.txt", "utf8", (err, data) => {
+    //     res.send(data);
+    // });
+    res.send(views.toString());
 });
 
 app.listen(process.env.PORT || 3000, () => { });
