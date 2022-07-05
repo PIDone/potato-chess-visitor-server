329657070e6a9e38558cced3a65aad7a0e6558be
anonymouscoder999 <jlche18659@gmail.com>
2022-07-05 13:29:41 +0800

    sdf

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 const express = require("express");
-const fs = require("fs");
-
 var views = 0;
-
 const app = express();
 
 app.get("/", (req, res) => {
     views++;
-    // fs.readFile("visitors.txt", "utf8", (err, data) => {
-        // let after = (parseInt(data) + 1).toString();
-        // fs.writeFile("visitors.txt", after, err => { });
-        res.send(views.toString());
-    // });
+    res.send(views.toString());
 });
 app.get("/query", (req, res) => {
-    // fs.readFile("visitors.txt", "utf8", (err, data) => {
-    //     res.send(data);
-    // });
     res.send(views.toString());
 });
 
