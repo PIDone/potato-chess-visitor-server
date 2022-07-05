18c5037768ecba6d8bd3fc63ade7b8e8a83b62df
anonymouscoder999 <jlche18659@gmail.com>
2022-07-05 08:01:10 +0800

    f

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ app.get("/", (req, res) => {
     fs.readFile("visitors.txt", "utf8", (err, data) => {
         let after = (parseInt(data) + 1).toString();
         fs.writeFile("visitors.txt", after, err => { });
-        res.send(after);
+        res.send(after + "<img src='./image.png' alt='dummy image'>");
+    });
+});
+app.post("/query", (req, res) => {
+    fs.readFile("visitors.txt", "utf8", (err, data) => {
+        res.send(data);
     });
 });
 
