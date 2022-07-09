64523b75761234536fb33681ca61f32154230ec9
anonymouscoder999 <jlche18659@gmail.com>
2022-07-09 11:04:46 +0800

    converted tabs to spaces

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var views = 0;
 const app = express();
 
 app.get("/", (req, res) => {
-    views++;
-    res.send(views.toString());
+	views++;
+	res.send(views.toString());
 });
 app.get("/query", (req, res) => {
-    res.send(views.toString());
+	res.send(views.toString());
 });
 
 app.listen(process.env.PORT || 3000, () => { });
